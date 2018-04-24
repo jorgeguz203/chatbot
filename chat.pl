e44f71837179ece62a8dee3606bf092cc63a206f
JORGE ARMANDO GUZMAN FLORES <A01208480@itesm.mx>
2018-04-23 22:25:55 -0500

    chat

--- chat.pl
+++ chat.pl
@@ -1,8 +1,31 @@
-:- [propuestas, templates, kb].
+    %<one line to give the program's name and a brief idea of what it does.>
+    %Copyright (C) 2018  Jorge Armando Guzman Flores
 
-chat:-writeln('Hola y bienvenido al chatbot de las elecciones, aqui podras encontrar informacion acerca de las propuestas
-	de los candidatos a la presidencia Mexico 2018. Menciona el candidato y el sector que te interesa sbaer?'),
-	readln(Input),
+    %This program is free software: you can redistribute it and/or modify
+    %it under the terms of the GNU General Public License as published by
+    %the Free Software Foundation, either version 3 of the License, or
+    %(at your option) any later version.
+
+    %This program is distributed in the hope that it will be useful,
+    %but WITHOUT ANY WARRANTY; without even the implied warranty of
+    %MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    %GNU General Public License for more details.
+
+    %You should have received a copy of the GNU General Public License
+    %along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+:- [propuestas, templates, kb]. %calls the files of the KBs
+
+%main function, this is the one that controls the inputs and send them to the logic
+chat:-writeln('chatPropuestas Copyright (C) 2018  Jorge Armando Guzman Flores
+    This program comes with ABSOLUTELY NO WARRANTY; for details type show w.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type show c for details.'),
+    writeln(''),
+	writeln('Hola y bienvenido al chatbot de las elecciones, aqui podras encontrar informacion acerca de las propuestas
+	de los candidatos a la presidencia Mexico 2018. Menciona el candidato y el sector que te interesa saber?
+	El bronco no entra en este chat debido a que sus propuestas fueron dadas despues'),
+	readln(Input), %makes the input a list of words
 	chat(Input),!.
 chat(Input):- Input == ['adios'],
 	writeln('Adios, espero haber sido de gran ayuda'), !.
@@ -14,65 +37,68 @@ chat(Input) :-
 	readln(Input1),
 	chat(Input1), !.
 
+%This section is just to see if the names of the candidates is correct
 chatMeade(X, R) :- meade(X), R = ['Favor de referise a', X, 'como meade'].
-
 chatAnaya(X, R) :- anaya(X), R = ['Favor de referise a', X, 'como anaya'].
-
 chatAmlo(X, R) :- amlo(X), R = ['Favor de referise a', X, 'como amlo'].
-
 chatMargarita(X, R) :- margarita(X), R = ['Favor de referise a', X, 'como margarita'].
 
+%this functions compare the keywords to see if they match to the kb.
 chatCandidato(X,R):- candidato(X) ->  R = ['No mencionase que sector te interesa saber de', X, '?' ];
-		sector(X) -> R = ['No mencionaste al candidato que te interesa saber del sector' , X].
-chatCandidato(X,R):- \+candidato(X), R = [X, 'no es un candidato o un sector'].
-
+		sector(X) -> R = ['No mencionaste al candidato que te interesa saber del sector' , X]. %if the keyword matches it send this response
+chatCandidato(X,R):- \+candidato(X), R = [X, 'no es un candidato o un sector']. %if does not match it sends this message
 chatSectores(X, R):-  sector(X), R = ['No mencionaste al candidato que te interesa saber del sector', X ].
 chatSectores(X, R):- \+sector(X), R = [X, 'no es un candidato o un sector'].
-
-chatPropuesta(X,Y, R):- propuesta(X,Y,Z), R = [Z]. 
+chatPropuesta(X,Y, R):- propuesta(X,Y,Z), R = [Z]. %Z is the proposal in the kb of propuesta, that is the aswer
 chatPropuesta(X,Y, R):- \+propuesta(X,Y,Z), R = ['No existe el candidato o el sector, puedes preguntar 
 	de nuevo, por los candidatos o los sectores, tambien puede ser que el candidato no tiene propuestas
-	de ese sector']. 
+	de ese sector o que te refieras a un candidato por otro nombre que no sea los siguientes: amlo,
+	anaya, meade o margarita']. 
 
+%this function is the one that compares the input with the templates
 match([],[]).
 match([], _):- true.
 
+%compare word by word to see if there is any matches
 match([S|Stim],[I|Input]) :- 
 	atom(S), 
 	S == I,
 	match(Stim, Input),!.
 
+%if there is no match it sends the contingency error
 match([S|Stim],[_|Input]) :-
 	\+atom(S),
 	match(Stim, Input),!.
 
+%This is where the keywords are changed to responses. 
 substitute([], _, _, Resp, R):- append(Resp, [], R),!.
 
-substitute([I|Index], Input, N, Resp, R):-
+%the following functions, take the keyword and send them to their respective function to compare that word in the kb
+substitute([I|_], Input, N, Resp, R):-
 	nth0(I, Input, Atom),
 	nth0(N, Resp, X),
 	X == flagCandidato,
 	chatMeade(Atom, R).
 
-substitute([I|Index], Input, N, Resp, R):-
+substitute([I|_], Input, N, Resp, R):-
 	nth0(I, Input, Atom),
 	nth0(N, Resp, X),
 	X == flagCandidato,
 	chatAnaya(Atom, R).
 
-substitute([I|Index], Input, N, Resp, R):-
+substitute([I|_], Input, N, Resp, R):-
 	nth0(I, Input, Atom),
 	nth0(N, Resp, X),
 	X == flagCandidato,
 	chatAmlo(Atom, R).
 
-substitute([I|Index], Input, N, Resp, R):-
+substitute([I|_], Input, N, Resp, R):-
 	nth0(I, Input, Atom),
 	nth0(N, Resp, X),
 	X == flagCandidato,
 	chatMargarita(Atom, R).
 
-substitute([I|Index], Input, N, Resp, R):-
+substitute([I|_], Input, N, Resp, R):-
 	nth0(I, Input, Atom),
 	nth0(N, Resp, X),
 	X == flagCandidato,
@@ -84,21 +110,25 @@ substitute([I|_], Input, _, Resp, R):-
 	X == flagSector,
 	chatSectores(Atom, R).
 
+%this work different from the others, because here we have 2 keywords in different pard of the list.
+%so its important to take the list and see how many keywords are in the list to send them in the correct order to the function
 substitute([I|Index], Input, N, Resp, R):-
-	length(Index, M), M =:= 0,
+	length(Index, M), M =:= 0, %if there is no more words in the list send the las keyword
 	nth0(I, Input, Atom),
 	nth0(N, Resp, X),
 	X == flagPropuesta,
 	chatPropuesta(_, Atom, R).
 
 substitute([I|Index], Input, N, Resp, R):-
-	length(Index, M), M > 0,
+	length(Index, M), M > 0, %while the list has more words keep searching the keyords
 	nth0(I, Input, Atom),
 	nth0(N, Resp, X),
 	X == flagPropuesta,
 	chatPropuesta(Atom, _, R),
 	substitute(Index, Input, N, Resp, R).
 
+%this is more simple, this just search for keywords that does not belong in a kb, so just take the sentence
+%the keyword and send a response putting the keyword where it belong in te resp
 substitute([I|Index], Input, N, Resp, R):-
 	nth0(I, Input, Atom),
 	select(N, Resp, Atom, R1), 
--- templates.pl
+++ templates.pl
@@ -11,20 +11,83 @@ template([cuales, son, los, candidatos], ['Los candidatos a la presidencia son: 
 template([cuales, son, los, candidatos, _], ['Los candidatos a la presidencia son: AMLO por Morena, Meade por el PRI, Anaya por el pan
 	Margarita como independiente y el Bronco igual como independiente pero este aun no tiene
 	propuestas. Quieres hablar de alguno en especial?'], []).
+template([dime, los, candidatos], ['Los candidatos a la presidencia son: AMLO por Morena, Meade por el PRI, Anaya por el pan
+	Margarita como independiente y el Bronco igual como independiente pero este aun no tiene
+	propuestas. Quieres hablar de alguno en especial?'], []).
+template([ayuda], ['para preguntar menciona el candidato y el sector deseado, puedes preguntar por los candidatos
+	o por los sectores'], []).
+template([como, debo, preguntar], ['Para tener una respuesta concreta es necesario mencionar el candidato 
+	y despues el sector deseado'], []).
+template([show, c], ['Go to https://www.gnu.org/licenses/gpl-3.0.en.html to know te terms and politics'], []).
+template([show, w], ['Go to https://www.gnu.org/licenses/gpl-3.0.en.html to know te terms and politics'], []).
 
 %Preguntas de sectores
-template([cuales, son, los, sectores], ['Los sectoresa a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
+template([cuales, son, los, sectores], ['Los sectores a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
 	social, seguridad, internacional o todos'], []).
-template([cuales, son, los, sectores, _], ['Los sectoresa a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
+template([cuales, son, los, sectores, _], ['Los sectores a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
 	social, seguridad, internacional o todos'],[]).
-template([que, son, los, sectores], ['Los sectoresa a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
+template([que, son, los, sectores], ['Los sectores a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
 	social, seguridad, internacional o todos'],[]).
-template([que, son, los, sectores, _], ['Los sectoresa a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
+template([que, son, los, sectores, _], ['Los sectores a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
 	social, seguridad, internacional o todos'],[]).
-template([sectores, ?], ['Los sectoresa a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
+template([sectores, ?], ['Los sectores a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
 	social, seguridad, internacional o todos'],[]).
-template([sectores, ?, _], ['Los sectoresa a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
+template([sectores, ?, _], ['Los sectores a los que las propuestan van drigidas son: corrupcion, economia, desarrollo, educacion,
 	social, seguridad, internacional o todos'],[]).
+template([candidato, ricardo, _], ['podrias referirte a ricardo anaya solo como anaya'], []).
+template([candidato, jose, _], ['podrias referirte a jose meade solo como meade'], []).
+template([candidato, antonio, _], ['podrias referirte a antonio meade solo como meade'], []).
+template([candidato, andres, _], ['podrias referirte a andres solo como amlo'], []).
+template([candidato, manuel, _], ['podrias referirte a manuel solo como amlo'], []).
+template([candidato, lopez, _], ['podrias referirte a lopez solo como amlo'], []).
+template([candidato, obrador, _], ['podrias referirte a obrador solo como amlo'], []).
+template([candidato, zavala, _], ['podrias referirte a zalava solo como margarita'], []).
+
+template([me, interesa, saber, sobre, el, sector, de, s(), del, candidato, s()], [flagPropuesta], [7, 10]).
+template([me, gustaria, saber, las, propuestas, de, s(), en, el, sector, s()], [flagPropuesta], [6, 10]).
+template([quiero, saber, las, propuestas, de, s(), en, el, sector, s()], [flagPropuesta], [5, 9]).
+template([me, interesa, saber, del, candidato, s(), sus, propuestas, en, s()], [flagPropuesta], [5, 9]).
+template([me, interesa, saber, del, candidato, s(), del, sector, de, s()], [flagPropuesta], [5, 9]).
+template([como, va, a, mejorar, la, s(), s()], [flagPropuesta], [5, 6]).
+template([como, va, a, terminar, la, s(), s()], [flagPropuesta], [5, 6]).
+template([quiero, saber, las, propuestas, de, s(), los, candidatos, en, el, sector, s()], [flagPropuesta], [5, 11]).
+template([me, gustaria, saber, de, s(), los, candidatos, el, sector, de, s()], [flagPropuesta], [4, 10]).
+template([quisiera, saber, propuestas, de, s(), del, candidato, s()], [flagPropuesta], [4, 7]).
+template([me, interesa, saber, de, s(), los, candidatos, el, sector, s()], [flagPropuesta], [4, 9]).
+template([que, propuesta, tiene, s(), para, combatir, la, s()], [flagPropuesta], [3, 7]).
+template([del, sector, de, s(), s(), los, candidatos], [flagPropuesta], [3, 4]).
+template([del, candidato, s(), en, el, sector, de, s()], [flagPropuesta], [2, 7]).
+template([del, candidato, s(), en, el, sector, s()], [flagPropuesta], [2, 6]).
+template([que, hara, s(), para, combatir, s()], [flagPropuesta], [2, 5]).
+template([que, hara, s(), para, combatir, la, s()], [flagPropuesta], [2, 6]).
+template([quisiera, saber, de, s(), los, candidatos, las, propuestas, del, sector, s()], [flagPropuesta], [3, 10]).
+template([que, propone, s(), en, s()], [flagPropuesta], [2, 4]).
+template([dime, las, s(), de, s()], [flagPropuesta], [2, 4]).
+template([quiero, saber, s(), las, propuestas, de, s()], [flagPropuesta], [2, 6]).
+template([dime, las, s(), de, s()], [flagPropuesta], [2, 4]).
+template([de, s(), el, sector, de, s()], [flagPropuesta], [1, 5]).
+template([de, s(), del, sector, s()], [flagPropuesta], [1, 4]).
+template([candidato, s(), sector, s()], [flagPropuesta], [1, 3]).
+template([sobre, s(), del, sector, s()], [flagPropuesta], [1, 4]).
+template([de, s(), s()], [flagPropuesta], [1, 2]).
+template([de, s(), los, candidatos, el, sector, de, s()], [flagPropuesta], [1, 7]).
+template([candidatos, s(), s()], [flagPropuesta], [1, 2]).
+template([sobre, s(), en, s(), los, sectores], [flagPropuesta], [1, 3]).
+template([sobre, s(), s(), las, propuestas], [flagPropuesta], [1, 2]).
+template([de, s(), en, s(), los, sectores], [flagPropuesta], [1, 3]).
+template([de, s(), s(), las, propuestas], [flagPropuesta], [1, 2]).
+template([del, candidato, s(), en, s(), los, sectores], [flagPropuesta], [2, 4]).
+template([del, candidato, s(), s(), las, propuestas], [flagPropuesta], [2, 3]).
+template([del, candidato, s(), s(), sus, propuestas], [flagPropuesta], [2,3]).
+template([cuales, son, s(), las, propuestas, de, s()], [flagPropuesta], [2,6]).
+template([cuales, son, las, s(), de, s()], [flagPropuesta], [3,5]).
+
+template([de, s(), los, candidatos, en, el, sector, s()], [flagPropuesta], [1,7]).
+template([de, s(), los, candidatos, en, el, sector, de, s()], [flagPropuesta], [1,8]).
+template([quiero, saber, de, s(), los, candidatos, en, el, sector, s()], [flagPropuesta], [3,9]).
+template([quiero, saber, de, s(), los, candidatos, en, el, sector, de, s()], [flagPropuesta], [3,10]).
+template([s(), los, candidatos, en, el, sector, s()], [flagPropuesta], [0,6]).
+
 
 template([hablemos, de, s()], [flagCandidato], [2]).
 template([quiero, hablar, de, s()], [flagCandidato], [3]).
@@ -42,27 +105,10 @@ template([de, las, propuestas, de, s(), _], [flagCandidato], [4]).
 template([me, gustaria, saber, de, s(), _], [flagCandidato], [4]).
 template([del , sector, de,  s()], [flagCandidato], [3]).
 
-template([de, s(), el, sector, de, s()], [flagPropuesta], [1, 5]).
-template([de, s(), del, sector, s()], [flagPropuesta], [1, 4]).
-template([candidato, s(), sector, s()], [flagPropuesta], [1, 3]).
-template([quiero, saber, las, propuestas, de, s(), en, el, sector, s()], [flagPropuesta], [5, 9]).
-template([me, gustaria, saber, las, propuestas, de, s(), en, el, sector, s()], [flagPropuesta], [6, 10]).
-template([sobre, s(), del, sector, s()], [flagPropuesta], [1, 4]).
-
-template([sobre, s(), en, s(), los, sectores], [flagPropuesta], [1, 3]).
-template([sobre, s(), s(), las, propuestas], [flagPropuesta], [1, 2]).
-template([de, s(), en, s(), los, sectores], [flagPropuesta], [1, 3]).
-template([de, s(), s(), las, propuestas], [flagPropuesta], [1, 2]).
-template([del, candidato, s(), en, s(), los, sectores], [flagPropuesta], [2, 4]).
-template([del, candidato, s(), s(), las, propuestas], [flagPropuesta], [2, 3]).
-
-template([de, s(), los, candidatos, en, el, sector, s()], [flagPropuesta], [1,7]).
-template([de, s(), los, candidatos, en, el, sector, de, s()], [flagPropuesta], [1,8]).
-template([quiero, saber, de, s(), los, candidatos, en, el, sector, s()], [flagPropuesta], [3,9]).
-template([quiero, saber, de, s(), los, candidatos, en, el, sector, de, s()], [flagPropuesta], [3,10]).
-template([s(), los, candidatos, en, el, sector, s()], [flagPropuesta], [0,6]).
-
-
+template([s(),s()], [flagPropuesta], [0,1]).
+template([s(), propuestas, de, s()], [flagPropuesta], [0, 3]).
+template([s(), los, candidatos, sobre, la, s()], [flagPropuesta], [0, 7]).
+template([s(), las, propuestas, de, s()], [flagPropuesta], [0, 4]).
 
 
 
